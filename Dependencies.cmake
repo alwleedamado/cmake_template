dd7bcfaafbd138b4e5b104e6df3ba49a90c67b6a
Alwaleed Ahmed <alwleedamado@gmail.com>
2026-01-08 20:31:00 +0200

    Update Dependencies.cmake

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -8,9 +8,9 @@ function(myproject_setup_dependencies)
   # For each dependency, see if it's
   # already been provided to us by a parent project
 
-  if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#11.1.4")
-  endif()
+  #if(NOT TARGET fmtlib::fmtlib)
+  #  cpmaddpackage("gh:fmtlib/fmt#11.1.4")
+  #endif()
 
   if(NOT TARGET spdlog::spdlog)
     cpmaddpackage(
@@ -28,13 +28,9 @@ function(myproject_setup_dependencies)
     cpmaddpackage("gh:catchorg/Catch2@3.8.1")
   endif()
 
-  if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.5.0")
-  endif()
-
-  if(NOT TARGET ftxui::screen)
-    cpmaddpackage("gh:ArthurSonzogni/FTXUI@6.0.2")
-  endif()
+  #if(NOT TARGET CLI11::CLI11)
+  #  cpmaddpackage("gh:CLIUtils/CLI11@2.5.0")
+  #endif()
 
   if(NOT TARGET tools::tools)
     cpmaddpackage("gh:lefticus/tools#update_build_system")
